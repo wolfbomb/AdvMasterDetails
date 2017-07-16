e6ccdf077b2c25eb73c80f01e917bb0e48517aa1
wolfbomb <nimrod.resulta@gmail.com>
2017-07-16 18:33:52 +0800

    Initital Commit

---
+++ AdvMasterDetails/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdvMasterDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getProductCategories()
+        {
+            List<Category> categories = new List<Category>();
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                categories = dc.Categories.OrderBy(a => a.CategortyName).ToList();
+            }
+            return new JsonResult { Data = categories, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult getProducts(int categoryID)
+        {
+            List<Product> products = new List<Product>();
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                products = dc.Products.Where(a => a.CategoryID.Equals(categoryID)).OrderBy(a => a.ProductName).ToList();
+            }
+            return new JsonResult { Data = products, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpPost]
+        public JsonResult save(OrderMaster order)
+        {
+            bool status = false;
+            DateTime dateOrg;
+            var isValidDate = DateTime.TryParseExact(order.OrderDateString, "mm-dd-yyyy", null, System.Globalization.DateTimeStyles.None, out dateOrg);
+            if (isValidDate)
+            {
+                order.OrderDate = dateOrg;
+            }
+
+            var isValidModel = TryUpdateModel(order);
+            if (isValidModel)
+            {
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    dc.OrderMasters.Add(order);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status} };
+        }
+	}
+}
